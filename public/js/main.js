537936032e2297af790c9055c314f29e81bc09d5
Barrett Quan <21barrett@gmail.com>
2017-02-09 12:36:37 -0800

    cleaned up and refactored lots of functions in main.js -need to fix search to work while empty

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,5 @@
 
 const $search_now_bttn = $('#search_now_Bttn');
-// $search.on('click', function(evt){
 
 function renderCard(result) {
   var listHeader =
@@ -42,87 +41,78 @@ function renderCard(result) {
   })
 }
 
+function getCoords(){
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var pos = {
+    lat: position.coords.latitude,
+    lng: position.coords.longitude
+    }
+    var str = pos.lat + ',' + pos.lng
+    return str;
+  })
+}
+
+function post(obj) {
+  $.post('/search', obj, (data) => {
+    renderCard(data);
+    swipe();
+    $('#advanced-button').remove();
+    $('#search_now_Bttn').remove();
+    $('#hide').remove();
+  })
+}
+
 const searchFunc = function(evt){
-  var $input = {
+
+  if ($("#locationInput").val() === '') {
+    str = getCoords();
+    var $input = {
+    location: str,
+    term: `food, ${$('#term').val().split(' '|| ',').join(',')}`,
+    price: $('#price').val()
+    };
+    console.log($input)
+    post($input);
+    }
+  else {
+    var $input = {
     location: $("#locationInput").val(),
     term: `food, ${$('#term').val().split(' '|| ',').join(',')}`,
-    price: $('#price').val()// ,
-  };  //if input field is blank, searches current location
-  console.log('clicked')
-  console.log($input)
-  if ($("#locationInput").val() === '') {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        }
-      $input.location = pos.lat + ',' + pos.lng;
-    $.post('/search', $input, (data) => {
-
-      renderCard(data);
-      // inserting hammer here onto cards
-      var myElement = document.querySelector('.card');
-
-      var swipeCard = new Hammer(myElement);
-
-      // // listen to events...
-      swipeCard.on("swiperight swipeleft tap press", function(ev) {
-          console.log(ev.type);
-      });
-
-// =============
-//     $.post('/search', $input, (data) => {
-//       var listHeader =
-//       `
-//       <div class="container">
-//           <h1 class="col-md-7">we think you might like...</h1>
-//           <div class="col-md-5 col-md-offset-1">
-//           <ul style="list-style: none;" id="list-header" class="col-md-8 col-md-offset-1"></ul>
-//       `
-//       $('#search').append(listHeader);
-//       console.log(data);
-//       data.forEach(function(biz) {
-//       console.log(biz.name)
-//       var restaurantName = biz.name;
-//       var image = biz.image_url;
-//       var id = biz.id;
-//       var rating = biz.rating_img_url;
-//       var review = biz.snippet_text;
-//       var yelpUrl = biz.url;
-//       var html =
-//         `
-//         <li>
-//           <div id="${id}" class="card" style="position: absolute; width: 100%; background: #bdbdbd; text-align: center">
-//             <img class="card-img-top" style="width: 25%" src="${image}" alt="yelp image">
-//             <div class="card-block">
-//               <h4 class="card-title"><a href="${yelpUrl}">${restaurantName}</a></h4>
-//               <img src="${rating}">
-//               <p class="card-text">${review}</p>
-//               <a href="#" class="btn btn-primary btn-lg">
-//                 <span class="glyphicon glyphicon-thumbs-up"></span>
-//               </a>
-//               <a href="#" class="btn btn-primary btn-lg">
-//                 <span class="glyphicon glyphicon-thumbs-down"></span>
-//               </a>
-//             </div>
-//           </div>
-//         </li>
-//         `
-//       $('#list-header').append(html);
-//       zIndex++;
-//       })
-// >>>>>>> 55f3551bc30407cda8e26bc8a7a8edf70754a3a6
-      $('#advanced-button').remove();
-      $('#hide').remove();
-     })
-    return position;
-    })
-  } else {
-      $.post('/search', $input, (data) => {
-        renderCard(data);
-    })
+    price: $('#price').val()
+    };
+    post($input);
   }
 }
+
+function swipe (evt) {
+  var myElement = this;
+  console.log('my element', myElement);
+  var swipeCard = new Hammer(myElement)
+  var last = document.querySelectorAll('.card')
+  var count = last.length - 1
+  swipeCard.on("swipe", function(evt) {
+    last[count].classList.toggle('hide')
+    count--;
+    console.log(evt.type)
+    if (count === 0) {
+      return console.log ('no more cards!')
+    }
+  })
+}
+
+$search_now_bttn.on('click', searchFunc);
+$('#adv_search_btn').on('click', searchFunc);
+
+//NOTE keeping as a swipe template example
+// var myElement = document.querySelector('.card');
+
+// var swipeCard = new Hammer(myElement);
+
+// // // listen to events...
+// swipeCard.on("swiperight swipeleft tap press", function(ev) {
+//     console.log(ev.type);
+// });
+
 
 // some request to get back every restaurant they liked
 // and then append html for each to "liked" page
@@ -149,8 +139,6 @@ const searchFunc = function(evt){
 
 //event listener for search button, when clicked
 //does yelp api post request
-$search_now_bttn.on('click', searchFunc);
-$('#adv_search_btn').on('click', searchFunc);
 
 // Bao(test) this will save to the database
 // var likebutton = ();
