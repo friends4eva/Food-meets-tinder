827094d1ebba2d32d26a0b95b836a8e2f80bedf2
Baokhoa Vu <tealkrysta92@gmail.com>
2017-02-08 17:33:49 -0800

    accessing embedded

--- db/seed.js
+++ db/seed.js
@@ -1,3 +1,15 @@
 require('./config')
 // Require models
+var User = require('../models/user')
 
+User.find().exec()
+  .then( users => {
+    var user = users[1]
+    return user.liked_businesses[2]
+  })
+  .then(business => {
+    console.log(business)
+  })
+  .then(name => {
+    // console.log(name)
+  })
