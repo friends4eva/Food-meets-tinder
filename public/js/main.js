55f3551bc30407cda8e26bc8a7a8edf70754a3a6
Lacey Madison <laceyannmadison@gmail.com>
2017-02-09 09:49:07 -0800

    stacked dem cards

--- public/js/main.js
+++ public/js/main.js
@@ -14,35 +14,46 @@ const searchFunc = function(evt){
         }
       $input.location = pos.lat + ',' + pos.lng;
     $.post('/search', $input, (data) => {
-      console.log(data[0]);
-      var restaurantName = data[0].name;
-      var image = data[0].image_url;
-      var id = data[0].id;
-      var rating = data[0].rating_img_url;
-      var review = data[0].snippet_text;
-      var yelpUrl = data[0].url;
-      var html = `
-        <div class="container">
+      var zIndex = 1;
+      var listHeader =
+      `
+      <div class="container">
           <h1 class="col-md-7">we think you might like...</h1>
           <div class="col-md-5 col-md-offset-1">
-            <div id="${id}" class="card" style="width: 100%; background: #bdbdbd; text-align: center">
-              <img class="card-img-top" style="width: 25%" src="${image}" alt="yelp image">
-              <div class="card-block">
-                <h4 class="card-title"><a href="${yelpUrl}">${restaurantName}</a></h4>
-                <img src="${rating}">
-                <p class="card-text">${review}</p>
-                <a href="#" class="btn btn-primary btn-lg">
-                  <span class="glyphicon glyphicon-thumbs-up"></span>
-                </a>
-                <a href="#" class="btn btn-primary btn-lg">
-                  <span class="glyphicon glyphicon-thumbs-down"></span>
-                </a>
-              </div>
+          <ul style="list-style: none;" id="list-header" class="col-md-8 col-md-offset-1"></ul>
+      `
+      $('#search').append(listHeader);
+      console.log(data);
+      data.forEach(function(biz) {
+      console.log(biz.name)
+      var restaurantName = biz.name;
+      var image = biz.image_url;
+      var id = biz.id;
+      var rating = biz.rating_img_url;
+      var review = biz.snippet_text;
+      var yelpUrl = biz.url;
+      var html =
+        `
+        <li>
+          <div id="${id}" class="card" style="position: absolute; width: 100%; background: #bdbdbd; text-align: center">
+            <img class="card-img-top" style="width: 25%" src="${image}" alt="yelp image">
+            <div class="card-block">
+              <h4 class="card-title"><a href="${yelpUrl}">${restaurantName}</a></h4>
+              <img src="${rating}">
+              <p class="card-text">${review}</p>
+              <a href="#" class="btn btn-primary btn-lg">
+                <span class="glyphicon glyphicon-thumbs-up"></span>
+              </a>
+              <a href="#" class="btn btn-primary btn-lg">
+                <span class="glyphicon glyphicon-thumbs-down"></span>
+              </a>
             </div>
           </div>
-        </div>
+        </li>
         `
-      $('#search').append(html);
+      $('#list-header').append(html);
+      zIndex++;
+      })
       $('#advanced-button').remove();
       $('#hide').remove();
      })
